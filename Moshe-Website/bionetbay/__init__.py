5ad39095de7e439b13328ad10851a632b83b8850
MosheSilverstein <moshechayimsilverstein@gmail.com>
2017-09-27 09:43:53 -0400

    Add files via upload

--- Moshe-Website/bionetbay/__init__.py
+++ Moshe-Website/bionetbay/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
 app.vars = {}
 
-from bionetbay import views
+from bionetbay import views, models
